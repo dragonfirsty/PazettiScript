a6757f0d9b45abda284fd734af2db8b480d82b98
Vinicius Silvestre <viniciusilvestre98@gmail.com>
2025-03-01 00:01:30 -0300

    Animação do programa

--- ListaLinear/ListaLinear/ListaLinear.cpp
+++ ListaLinear/ListaLinear/ListaLinear.cpp
@@ -1,7 +1,19 @@
 // Claudio Souza Nunes
 
 #include <iostream>
+#include <thread>   
+#include <limits>  
+#include <cstdlib>  
 using namespace std;
+
+void limparTela() {
+#ifdef _WIN32
+	system("cls"); // Windows
+#else
+	system("clear"); // Linux/macOS
+#endif
+}
+
 
 // headers
 void menu();
@@ -16,19 +28,39 @@ void maiorNumero();
 void soma10numeros();
 void idades10();
 void explicacoes();
+void exibirArteAsciiComAnimacao() {
+	std::string arteAscii[] = {
+		" _____                          _    _   _____              _         _ ",
+		"|  __ \\                        | |  (_) / ____|            (_)       | |",
+		"| |__) |  __ _  ____ ____  ___ | |_  _ | (___    ___  _ __  _  _ __  | |_",
+		"|  ___/  / _` ||_  /|_  / / _ \\| __|| | \\___ \\  / __|| '__|| || '_ \\ | __|",
+		"| |     | (_| | / /  / / |  __/| |_ | | ____) || (__ | |   | || |_) || |_",
+		"|_|      \\__,_|/___|/___| \\___| \\__||_||_____/  \\___||_|   |_|| .__/  \\__|",
+		"                                                              | |",
+		"                                                              |_|"
+	};
+	for (const std::string& linha : arteAscii) {
+		std::cout << linha << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(200)); 
+	}
+}
+
 //--------------------------
 
 
 int main(){
-
+	exibirArteAsciiComAnimacao();
+	limparTela();
 	setlocale(LC_ALL, "pt_BR.UTF-8");
 	menu();
 }
 
 void menu(){
+	
 
 	int escolhaMenu = 0;
 	while (escolhaMenu != 12) {
+
 		system("cls"); // somente no windows
 		cout << "Menu PazettiScript";
 		cout << endl << endl;
@@ -86,7 +118,6 @@ void explicacoes(){
 	int escolha = 0;
 
 	cout << "Qual desafio voce quer receber a explicacao?" << endl;
-	
 
 
 
